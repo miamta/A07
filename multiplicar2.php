3df3a68326a613ee2d8deb560cc7dab710f453bc
Your Name <you@example.com>
2016-10-20 09:47:01 +0200

    primer commit

---
+++ multiplicar2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> A07 </title>
+  </head>
+  <body>
+    <?php
+$numero=1;
+echo "Tabla de 2: <br>";
+while ($numero <= 10) {
+  $tabla=$numero*2;
+  echo "2 * $numero = $tabla <br>";
+  $numero++;
+}
+
+
+
+
+
+
+
+
+       ?>
+
+  </body>
+</html>
